c77635a044772f977ffcf55fa635eb3f071ae0f8
Pedro <estebantordesillaspedro@gmail.com>
2022-06-08 13:50:25 +0200

    cambios condicion cantidad preguntas y temporizador

--- src/components/Reloj.js
+++ src/components/Reloj.js
@@ -46,7 +46,7 @@ const Reloj = () => {
     const getDeadTime = () => {
         let deadline = new Date();
 
-        deadline.setSeconds(deadline.getSeconds() + 10);
+        deadline.setSeconds(deadline.getSeconds() + 60);
         return deadline;
     }
 
